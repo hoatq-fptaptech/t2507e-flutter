b080ef765227c5d7edbe692250512b13d7ac6e08
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-09-21 19:10:39 +0700

    demo state management with provider

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -3,10 +3,15 @@ import 'package:t2507e_flutter/models/category_model.dart';
 
 class CartProvider extends ChangeNotifier{
   final List<Category> _items = [];
+  String _jwtToken = "";
 
   List<Category> get items => _items;
   int get count => _items.length;
-
+  String get jwt => _jwtToken;
+  void setJwtToken(String token){
+    _jwtToken = token;
+    // notifyListeners();
+  }
   double get subtotal {
     return 1500.0;
   }
